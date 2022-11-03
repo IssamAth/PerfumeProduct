c7f4e5a16717cea204023c137cb4b002e28b36ba
Issam Athamena <samo.alger@gmail.com>
2022-11-03 15:12:50 +0100

    there is a bug in displaying IMG2

--- src/components/productImage/ProductImage.jsx
+++ src/components/productImage/ProductImage.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import './productImage.css'
 import IMG from '../../assets/image-product-mobile.jpg'
+import IMG2 from '../../assets/image-product-desktop.jpg'
 
 const ProductImage = () => {
+  function displayImage(){
+    let vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+    console.log(vw);
+    if(vw < 600) {
+     console.log(vw + "is smaller than 600");
+     return (<img src={IMG} alt="" />);
+    } else {
+     console.log(vw + "is bigger than 600");
+     return (<img src={IMG2} alt="" />);
+    }
+}
   return (
     <div className='container-img'>
-       <img src={IMG} alt="" />
+      {displayImage()}
     </div>
   )
 }
